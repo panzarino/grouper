d864401ac3642aed6d439f29ec84c24a6673ca05
Zach Panzarino <zachary@panzarino.com>
2016-05-13 18:24:29 +0000

    Replace creator with selector

---
+++ src/main/java/hello/Selector.java
@@ -0,0 +1,53 @@
+package hello;
+
+import java.sql.ResultSet;
+
+/**
+ * Class that selects from the database
+ * @author Zach Panzarino
+ * @version 1.0.0
+ */
+public class Selector extends Worker {
+    /**
+     * Creates a new Selector with a database connection
+     * @param url The database URL
+     * @param username The database username
+     * @param password The database password
+     */
+    public CreateWorker(String url, String username, String password){
+        super(url, username, password);
+    }
+    /**
+     * Select from database
+     * @param select What to be selected
+     * @param from What table to use
+     * @return Results of executed query
+     */
+    public ResultSet select(String select, String from){
+        String query = "SELECT `"+select+"` FROM  `"+from+"`";
+        return execute(query);
+    }
+    /**
+     * Select from database
+     * @param select What to be selected
+     * @param from What table to use
+     * @param where Condition for selection
+     * @return Results of executed query
+     */
+    public ResultSet select(String select, String from, String where){
+        String query = "SELECT `"+select+"` FROM  `"+from+"` WHERE "+where;
+        return execute(query);
+    }
+    /**
+     * Select from database
+     * @param select What to be selected
+     * @param from What table to use
+     * @param where Condition for selection
+     * @param limit Limit for selection
+     * @return Results of executed query
+     */
+    public ResultSet select(String select, String from, String where, String limit){
+        String query = "SELECT `"+select+"` FROM  `"+from+"` WHERE "+where+" LIMIT "+limit;
+        return execute(query);
+    }
+}
