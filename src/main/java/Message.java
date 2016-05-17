f5b3b215a74644e760ec595e152aca033a4d3987
Zach Panzarino <zachary@panzarino.com>
2016-05-17 18:15:10 +0000

    Allocate actions for messages

--- src/main/java/Message.java
+++ src/main/java/Message.java
@@ -6,27 +6,41 @@ package hello;
  * @version 1.0.0
  */
 public class Message {
-    private String text;
+    private String number, text;
     /**
      * Creates a new Message and stores the text
+     * @param num Sending phone number
      * @param content The content of the message
      */
-    public Message(String content){
+    public Message(String num, String content){
+        number = num;
         text = content;
     }
     /**
      * Determines what to do with a message
      * @return Whether the message was successfully executed
      */
-    public boolean execute(){
+    public void execute(){
         if (text.startsWith("/")){
             int space = text.indexOf(" ");
-            if (space == -1)
-                return false;
-            String command = text.substring(1, space);
-            String content = text.substring(space+1);
-            return true;
+            if (space != 1){
+                String command = text.substring(1, space);
+                String content = text.substring(space+1);
+                if (command.equals("create")){
+                    Action.create(number, content);
+                    return;
+                }
+                if (command.equals("join")){
+                    Action.join(number, content);
+                    return;
+                }
+                if (command.equals("leave")){
+                    Action.leave(number);
+                    return;
+                }
+            }
         }
-        return true;
+        Action.message(number, text);
+        return;
     }
 }
