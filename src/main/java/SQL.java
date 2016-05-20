9bd38806bd11fa11741ba39e81d3837e8a437e4e
Zach Panzarino <zachary@panzarino.com>
2016-05-20 17:55:29 +0000

    Update username

--- src/main/java/SQL.java
+++ src/main/java/SQL.java
@@ -1,6 +1,6 @@
 package hello;
 
 public class SQL {
-    public static final String username = "zachpanz88";
+    public static final String username = "jakesyl1";
     public static final String password = null;
 }
